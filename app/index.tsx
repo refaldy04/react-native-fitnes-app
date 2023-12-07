eee4902b1b7139c21c5dee36dc259d6d2b6e5d56
Refaldy <refaldybanggana@gmail.com>
2023-12-07 10:27:29 +0700

    feat: welcome screen

---
+++ app/index.tsx
@@ -0,0 +1,62 @@
+import { View, Text, Image, TouchableOpacity } from "react-native";
+import React from "react";
+import {
+  widthPercentageToDP as wp,
+  heightPercentageToDP as hp,
+} from "react-native-responsive-screen";
+import { StatusBar } from "expo-status-bar";
+import { LinearGradient } from "expo-linear-gradient";
+import Animated, { FadeIn, FadeInDown, FadeOut } from "react-native-reanimated";
+
+export default function App() {
+  return (
+    <View className="flex-1 justify-end">
+      <StatusBar style="light" />
+      <Image
+        className="w-full h-full absolute"
+        source={require("../assets/images/welcome.png")}
+      />
+
+      <LinearGradient
+        colors={["transparent", "#18181b"]}
+        style={{ width: wp(100), height: hp(100) }}
+        start={{ x: 0.5, y: 0 }}
+        end={{ x: 0.5, y: 0.8 }}
+        className="justify-end pb-12 space-y-8"
+      >
+        <Animated.View
+          entering={FadeInDown.delay(100).springify()}
+          className="items-center"
+        >
+          <Text
+            style={{ fontSize: hp(5) }}
+            className="text-white font-bold tracking-wide"
+          >
+            Best
+            <Text className="text-rose-500"> Workouts</Text>
+          </Text>
+          <Text
+            style={{ fontSize: hp(5) }}
+            className="text-white font-bold tracking-wide"
+          >
+            For You
+          </Text>
+        </Animated.View>
+
+        <Animated.View entering={FadeInDown.delay(200).springify()}>
+          <TouchableOpacity
+            style={{ height: hp(7), width: wp(80) }}
+            className="bg-rose-500 items-center justify-center rounded-full mx-auto border-[2px] border-neutral-200"
+          >
+            <Text
+              style={{ fontSize: hp(3) }}
+              className="text-white font-bold tracking-widest"
+            >
+              Get Started
+            </Text>
+          </TouchableOpacity>
+        </Animated.View>
+      </LinearGradient>
+    </View>
+  );
+}
